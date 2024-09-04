af1a64413599432d2ace78fd5da745ad5765c44c
FurkannOzbek <furkannozbek@yandex.com>
2024-09-04 14:16:44 +0200

    isActive added

--- components/ui/NavItem.js
+++ components/ui/NavItem.js
@@ -1,15 +1,12 @@
 import Link from "next/link";
 import classNames from "classnames";
 import styles from "./Navbar.module.css";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
 
 export const NavItem = ({ title, link, isActive, index }) => {
-  const currentPath = usePathname();
   return (
     <li
       className={classNames(styles.navbarLinks, {
-        [styles.isLinkActive]: link === currentPath,
+        [styles.isLinkActive]: isActive,
       })}
     >
       <Link href={link}>
--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -1,9 +1,7 @@
 "use client";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
-import Link from "next/link";
 import { NavItem } from "./NavItem";
-
 import styles from "./Navbar.module.css";
 
 const navbarItems = [
@@ -22,6 +20,7 @@ const navbarItems = [
 ];
 
 export const Navbar = () => {
+  const currentPath = usePathname();
   return (
     <header className={styles.headerContainer}>
       <div className={styles.navbarLogo}>
@@ -34,9 +33,24 @@ export const Navbar = () => {
         <div className={styles.navbarBG} />
         <ul className={styles.navbarList}>
           {/* when we will use map function next week, that string indexes will be fixed, this is just temporary*/}
-          <NavItem title={navbarItems[0].title} link={navbarItems[0].link} index="01" />
-          <NavItem title={navbarItems[1].title} link={navbarItems[1].link} index="02" />
-          <NavItem title={navbarItems[2].title} link={navbarItems[2].link} index="03" />
+          <NavItem
+            title={navbarItems[0].title}
+            link={navbarItems[0].link}
+            index="01"
+            isActive={navbarItems[0].link === currentPath}
+          />
+          <NavItem
+            title={navbarItems[1].title}
+            link={navbarItems[1].link}
+            index="02"
+            isActive={navbarItems[1].link === currentPath}
+          />
+          <NavItem
+            title={navbarItems[2].title}
+            link={navbarItems[2].link}
+            index="03"
+            isActive={navbarItems[2].link === currentPath}
+          />
 
           {/* TASK - React 1 week 3 */}
           {/* replace repeating content by using navbarItems.map(() => <NavLink />) */}
