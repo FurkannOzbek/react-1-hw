6457655798e3ed72b7b55541d1516c52816671cf
FurkannOzbek <furkannozbek@yandex.com>
2024-09-04 17:53:55 +0200

    small fix

--- components/ui/SocialMedieItem.js
+++ components/ui/SocialMedieItem.js
@@ -1,11 +1,8 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-
 export const SocialMediaItem = ({ url, title, icon }) => {
   return (
     <li style={{ display: "flex", alignItems: "center" }}>
       <img src={icon} height="20px" width="20px" alt={title} />
-      <a href={url} style={{ marginBottom: "0", marginLeft: "0.5em" }}>
+      <a href={url} style={{ marginLeft: "0.5em" }}>
         {title}
       </a>
     </li>
