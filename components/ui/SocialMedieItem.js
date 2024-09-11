4e7562bb6789503d2a748d86cb6362cbb50f268b
FurkannOzbek <furkannozbek@yandex.com>
2024-09-11 23:08:01 +0200

    fix v4

--- components/ui/SocialMedieItem.js
+++ components/ui/SocialMedieItem.js
@@ -1,7 +1,7 @@
 export const SocialMediaItem = ({ url, title, icon }) => {
   return (
     <li style={{ display: "flex", alignItems: "center" }}>
-      <img src={icon} height="20px" width="20px" alt={title} />
+      <img src={icon} height="20px" width="20px" alt="" />
       <a href={url} style={{ marginLeft: "0.5em" }}>
         {title}
       </a>
